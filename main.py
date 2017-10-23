c82d40c8f0af4ff3299c23b03d657d25339d1668
Linus <linuzlp@gmail.com>
2017-10-23 17:44:36 +0200

    Connected with the API

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-
+import tweepy
+import time
 
 # Gets the keys from my key file
 # Won't upload that for obvious reasons.
  
 from keys import *
+
+# Initial setup and connecting to the API
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+api = tweepy.API(auth)
+
+#TODO: Make some shit
