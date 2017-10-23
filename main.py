ab05426a11d27a352989e7e2d6f93961850393ff
Linus <linuzlp@gmail.com>
2017-10-23 22:21:21 +0200

    First working stage! :cucumber:

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tweepy
 import time
+import schedule
+import markovify
 
-# Gets the keys from my key file
 # Won't upload that for obvious reasons.
  
 from keys import *
@@ -13,4 +14,36 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
 api = tweepy.API(auth)
 
-#TODO: Make some shit
+# Main bot class
+
+class PickleBot(object):
+    def __init__(self):
+        print("### PickleBot 1.0 up and running ###")
+    
+    def generate_tweet(self):
+        with open("rick_transcripts.txt") as f:
+            text = f.read()
+            f.close()
+        text_model = markovify.Text(text)
+        return(text_model.make_short_sentence(140))
+
+    def post_tweet(self):
+        tweet = self.generate_tweet()
+        api.update_status(tweet)
+        print(tweet)
+        print(time.ctime())
+
+
+if __name__ == '__main__':
+    p = PickleBot()
+
+    # Sends initial tweet
+    p.post_tweet()
+
+    # Sets up a schedule
+    schedule.every(15).minutes.do(p.post_tweet())
+
+    # Runs the schedule for timed posts
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
